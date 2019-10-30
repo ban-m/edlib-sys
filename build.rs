f7426ad869af0a5a0283e6f688af42f5c0b8d7a3
ban-m <banmasutani@gmail.com>
2019-10-30 16:25:22 +0900

    support for mac os

--- build.rs
+++ build.rs
@@ -4,18 +4,33 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    println!("{:?}",cc::Build::new().get_compiler().path());
-    cc::Build::new()
-        .cpp(true)
-        .warnings(true)
-        .cpp_link_stdlib("stdc++")
-        .flag("-Wall")
-        .flag("-Wextra")
-        .flag("-std=c++11")
-        .flag("-g")
-        .include("src/edlib/include")
-        .file("src/edlib/src/edlib.cpp")
-        .compile("libedlib.a");
+    println!("{:?}", cc::Build::new().get_compiler().path());
+    if cfg!(target_os = "linux") {
+        cc::Build::new()
+            .cpp(true)
+            .warnings(true)
+            .cpp_link_stdlib("stdc++")
+            .flag("-Wall")
+            .flag("-Wextra")
+            .flag("-std=c++11")
+            .flag("-g")
+            .include("src/edlib/include")
+            .file("src/edlib/src/edlib.cpp")
+            .compile("libedlib.a");
+    }
+    if cfg!(target_os = "macos") {
+        cc::Build::new()
+            .cpp(true)
+            .warnings(true)
+            .cpp_link_stdlib("c++")
+            .flag("-Wall")
+            .flag("-Wextra")
+            .flag("-std=c++11")
+            .flag("-g")
+            .include("src/edlib/include")
+            .file("src/edlib/src/edlib.cpp")
+            .compile("libedlib.a");
+    }
     let bindings = bindgen::Builder::default()
         .header("src/edlib/include/edlib.h")
         .generate()
