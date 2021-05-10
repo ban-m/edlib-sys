d2eb2622d6c1c0f577ba808239da1fd7f10fcc42
ban-m <ban-m@g.ecc.u-tokyo.ac.jp>
2021-05-10 14:11:59 +0900

    wrote full API

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,85 @@ mod tests {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Alignment {
+    pub dist: u32,
+    /// Pair of optimal alignments.
+    pub locations: Option<Vec<(usize, usize)>>,
+    /// Alignment operations.0=>Match, 1=> Ins, 2=>Del 3=> Mism
+    pub operations: Option<Vec<u8>>,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum AlignMode {
+    Global,
+    Prefix,
+    Infix,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum AlignTask {
+    Distance,
+    Location,
+    Alignment,
+}
+
+pub fn edlib_align(target: &[u8], query: &[u8], mode: AlignMode, task: AlignTask) -> Alignment {
+    let mode_ed = match mode {
+        AlignMode::Global => EdlibAlignMode_EDLIB_MODE_NW,
+        AlignMode::Prefix => EdlibAlignMode_EDLIB_MODE_SHW,
+        AlignMode::Infix => EdlibAlignMode_EDLIB_MODE_HW,
+    };
+    let task_ed = match task {
+        AlignTask::Distance => EdlibAlignTask_EDLIB_TASK_DISTANCE,
+        AlignTask::Location => EdlibAlignTask_EDLIB_TASK_LOC,
+        AlignTask::Alignment => EdlibAlignTask_EDLIB_TASK_PATH,
+    };
+    unsafe {
+        let config = edlibNewAlignConfig(-1, mode_ed, task_ed, std::ptr::null_mut(), 0);
+        let align = edlibAlign(
+            query.as_ptr() as *const i8,
+            query.len() as i32,
+            target.as_ptr() as *const i8,
+            target.len() as i32,
+            config,
+        );
+        if align.status != 0 {
+            panic!("Edlib paniced!");
+        } else {
+            let dist = align.editDistance as u32;
+            let locations: Option<Vec<(usize, usize)>> = (task != AlignTask::Distance).then(|| {
+                let length = align.numLocations as usize;
+                let starts = std::slice::from_raw_parts(align.startLocations, length).to_vec();
+                let ends = std::slice::from_raw_parts(align.startLocations, length).to_vec();
+                starts
+                    .iter()
+                    .zip(ends)
+                    .map(|(&x, y)| (x as usize, y as usize))
+                    .collect()
+            });
+            let operations = (task == AlignTask::Alignment).then(|| {
+                let length = align.alignmentLength as usize;
+                std::slice::from_raw_parts(align.alignment, length).to_vec()
+            });
+            edlibFreeAlignResult(align);
+            Alignment {
+                dist,
+                locations,
+                operations,
+            }
+        }
+    }
+}
+
 pub fn global_dist(target: &[u8], query: &[u8]) -> u32 {
     global_dist_k(target, query, -1)
 }
