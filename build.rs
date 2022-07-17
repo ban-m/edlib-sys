7ab12d0423d753ce4a7694f2a13cd42c2c75918f
Bansho Masutani <banmasutani@gmail.com>
2022-07-17 13:05:58 +0900

    refine bindings & remove bindgen

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-use std::env;
-use std::path::PathBuf;
+// use std::env;
+// use std::path::PathBuf;
 
 fn main() {
     println!("{:?}", cc::Build::new().get_compiler().path());
@@ -44,13 +44,4 @@ fn main() {
             .file("src/edlib/src/edlib.cpp")
             .compile("libedlib.a");
     }
-    let bindings = bindgen::Builder::default()
-        .header("src/edlib/include/edlib.h")
-        .generate()
-        .expect("Unable to generate bindings");
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,33 +2,42 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+pub mod bindings;
 
 #[derive(Debug, Clone)]
 pub struct Alignment {
-    pub dist: u32,
-    /// Pair of optimal alignments.
-    pub locations: Option<Vec<(usize, usize)>>,
-    /// Alignment operations.0=>Match, 1=> Ins, 2=>Del 3=> Mism
-    /// Note that leading/trailing sequence in the query sequence
-    /// would not apprear in this operation. Use .locations to obtain the full alignment.
-    pub operations: Option<Vec<u8>>,
+    align: bindings::EdlibAlignResult,
+    task: AlignTask,
+    mode: AlignMode,
+}
+
+impl Alignment {
+    pub fn dist(&self) -> u32 {
+        self.align.editDistance as u32
+    }
+    /// Note that the end coordinate is *1-index*!
+    pub fn location(&self) -> Option<(usize, usize)> {
+        (self.task != AlignTask::Distance).then(|| unsafe {
+            let length = self.align.numLocations as usize;
+            let starts = std::slice::from_raw_parts(self.align.startLocations, length);
+            let ends = std::slice::from_raw_parts(self.align.endLocations, length);
+            (starts[0] as usize, ends[0] as usize)
+        })
+    }
+    pub fn operations(&self) -> Option<&[u8]> {
+        (self.task == AlignTask::Alignment).then(|| unsafe {
+            let length = self.align.alignmentLength as usize;
+            std::slice::from_raw_parts(self.align.alignment, length)
+        })
+    }
+}
+
+impl Drop for Alignment {
+    fn drop(&mut self) {
+        unsafe { bindings::edlibFreeAlignResult(self.align) };
+    }
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -45,20 +54,20 @@ pub enum AlignTask {
     Alignment,
 }
 
-pub fn edlib_align(query: &[u8], target: &[u8], mode: AlignMode, task: AlignTask) -> Alignment {
+pub fn align(query: &[u8], target: &[u8], mode: AlignMode, task: AlignTask) -> Alignment {
     let mode_ed = match mode {
-        AlignMode::Global => EdlibAlignMode_EDLIB_MODE_NW,
-        AlignMode::Prefix => EdlibAlignMode_EDLIB_MODE_SHW,
-        AlignMode::Infix => EdlibAlignMode_EDLIB_MODE_HW,
+        AlignMode::Global => bindings::EdlibAlignMode_EDLIB_MODE_NW,
+        AlignMode::Prefix => bindings::EdlibAlignMode_EDLIB_MODE_SHW,
+        AlignMode::Infix => bindings::EdlibAlignMode_EDLIB_MODE_HW,
     };
     let task_ed = match task {
-        AlignTask::Distance => EdlibAlignTask_EDLIB_TASK_DISTANCE,
-        AlignTask::Location => EdlibAlignTask_EDLIB_TASK_LOC,
-        AlignTask::Alignment => EdlibAlignTask_EDLIB_TASK_PATH,
+        AlignTask::Distance => bindings::EdlibAlignTask_EDLIB_TASK_DISTANCE,
+        AlignTask::Location => bindings::EdlibAlignTask_EDLIB_TASK_LOC,
+        AlignTask::Alignment => bindings::EdlibAlignTask_EDLIB_TASK_PATH,
     };
     unsafe {
-        let config = edlibNewAlignConfig(-1, mode_ed, task_ed, std::ptr::null_mut(), 0);
-        let align = edlibAlign(
+        let config = bindings::edlibNewAlignConfig(-1, mode_ed, task_ed, std::ptr::null_mut(), 0);
+        let align = bindings::edlibAlign(
             query.as_ptr() as *const i8,
             query.len() as i32,
             target.as_ptr() as *const i8,
@@ -68,128 +77,22 @@ pub fn edlib_align(query: &[u8], target: &[u8], mode: AlignMode, task: AlignTask
         if align.status != 0 {
             panic!("Edlib paniced!");
         } else {
-            let dist = align.editDistance as u32;
-            let locations: Option<Vec<(usize, usize)>> = (task != AlignTask::Distance).then(|| {
-                let length = align.numLocations as usize;
-                let starts = std::slice::from_raw_parts(align.startLocations, length).to_vec();
-                let ends = std::slice::from_raw_parts(align.endLocations, length).to_vec();
-                starts
-                    .iter()
-                    .zip(ends)
-                    .map(|(&x, y)| (x as usize, y as usize))
-                    .collect()
-            });
-            let operations = (task == AlignTask::Alignment).then(|| {
-                let length = align.alignmentLength as usize;
-                std::slice::from_raw_parts(align.alignment, length).to_vec()
-            });
-            edlibFreeAlignResult(align);
-            Alignment {
-                dist,
-                locations,
-                operations,
-            }
+            Alignment { align, task, mode }
         }
     }
 }
 
-pub fn global_dist(target: &[u8], query: &[u8]) -> u32 {
-    global_dist_k(target, query, -1)
-}
-
-pub fn global_dist_k(target: &[u8], query: &[u8], k: i32) -> u32 {
-    unsafe {
-        let config = edlibNewAlignConfig(
-            k,
-            EdlibAlignMode_EDLIB_MODE_NW,
-            EdlibAlignTask_EDLIB_TASK_DISTANCE,
-            std::ptr::null_mut(),
-            0,
-        );
-        let align = edlibAlign(
-            query.as_ptr() as *const i8,
-            query.len() as i32,
-            target.as_ptr() as *const i8,
-            target.len() as i32,
-            config,
-        );
-        let result = align.editDistance as u32;
-        edlibFreeAlignResult(align);
-        result
-    }
-}
-
-/// Calculate the global alignment for given query and reference.
-/// 0 for match, 1 for insertion to target, 2 for insertion to query, and 3 for mismatch.
-pub fn global(target: &[u8], query: &[u8]) -> Vec<u8> {
-    unsafe {
-        let config = edlibNewAlignConfig(
-            -1,
-            EdlibAlignMode_EDLIB_MODE_NW,
-            EdlibAlignTask_EDLIB_TASK_PATH,
-            std::ptr::null_mut(),
-            0,
-        );
-        let align = edlibAlign(
-            query.as_ptr() as *const i8,
-            query.len() as i32,
-            target.as_ptr() as *const i8,
-            target.len() as i32,
-            config,
-        );
-        let result =
-            std::slice::from_raw_parts(align.alignment, align.alignmentLength as usize).to_vec();
-        edlibFreeAlignResult(align);
-        result
-    }
-}
-
-pub fn local(target: &[u8], query: &[u8]) -> Vec<u8> {
-    unsafe {
-        let config = edlibNewAlignConfig(
-            -1,
-            EdlibAlignMode_EDLIB_MODE_HW,
-            EdlibAlignTask_EDLIB_TASK_PATH,
-            std::ptr::null_mut(),
-            0,
-        );
-        let align = edlibAlign(
-            query.as_ptr() as *const i8,
-            query.len() as i32,
-            target.as_ptr() as *const i8,
-            target.len() as i32,
-            config,
-        );
-        let result =
-            std::slice::from_raw_parts(align.alignment, align.alignmentLength as usize).to_vec();
-        edlibFreeAlignResult(align);
-        result
-    }
-}
-
-pub fn align(query: &[u8], refr: &[u8]) -> String {
-    unsafe {
-        let config = edlibNewAlignConfig(
-            -1,
-            EdlibAlignMode_EDLIB_MODE_SHW,
-            EdlibAlignTask_EDLIB_TASK_PATH,
-            std::ptr::null_mut(),
-            0,
-        );
-        let align = edlibAlign(
-            query.as_ptr() as *const i8,
-            query.len() as i32,
-            refr.as_ptr() as *const i8,
-            refr.len() as i32,
-            config,
-        );
-        let cigar = edlibAlignmentToCigar(
-            align.alignment,
-            align.alignmentLength,
-            EdlibCigarFormat_EDLIB_CIGAR_EXTENDED,
-        );
-        edlibFreeAlignResult(align);
-        let cigar = std::ffi::CString::from_raw(cigar);
-        cigar.to_string_lossy().to_string()
+#[cfg(test)]
+mod test {
+    use crate::{align, AlignMode, AlignTask};
+    #[test]
+    fn edlib_test() {
+        let target = b"AAACCCTTT";
+        let query = b"CCC";
+        let task = AlignTask::Alignment;
+        let mode = AlignMode::Infix;
+        let aln = align(query, target, mode, task);
+        assert_eq!(aln.dist(), 0);
+        assert_eq!(aln.operations(), Some([0, 0, 0].as_slice()));
     }
 }
